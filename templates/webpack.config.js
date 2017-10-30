a8825fb3e4779a13f11734182a100eba7c874bca
Michał Sobiecki <michalsobiecki@yahoo.com>
2017-10-30 17:09:29 +0100

    Fix

--- templates/webpack.config.js
+++ templates/webpack.config.js
@@ -80,11 +80,6 @@ const cssDevelop = [{
 }, {
 	loader: 'sass-resources-loader',
 	options: {
-		resources: ['./source/utils/_clearfix.sass']
-	},
-}, {
-	loader: 'sass-resources-loader',
-	options: {
 		resources: [paths.utilsPath + '_clearfix.sass', paths.utilsPath + '_colors.sass', paths.utilsPath + '_rem.sass', paths.utilsPath + '_responsive.sass', paths.utilsPath + '_typography.sass', paths.utilsPath + '_cms.sass']
 	},
 }];
