6d7fc366a9c3e56d34a70965a15829bd64322a40
Michał Sobiecki <michalsobiecki@yahoo.com>
2017-10-30 16:53:37 +0100

    Save

---
+++ templates/source/workshop/layout/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './Footer.sass';
+
+class Footer extends Component {
+	render() {
+		return (
+			<footer>
+			</footer>
+		);
+	}
+}
+
+export default Footer;
